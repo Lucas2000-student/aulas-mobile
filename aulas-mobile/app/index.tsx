9ea95d12dc92e678eebed0724a3da04ae7c6a74d
Lucas2000-student <rm560179@fiap.com.br>
2026-04-09 21:42:03 -0300

    Dia 04/09

--- aulas-mobile/app/index.tsx
+++ aulas-mobile/app/index.tsx
@@ -3,12 +3,49 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { useAuth } from "@/hooks/useAuth";
 import { View } from "react-native";
+import MapView from 'react-native-maps'
+import * as batata from 'expo-location'
+import { useEffect, useState } from "react";
 
 export default function Home() {
     const { email, setEmail, password, setPassword, login, signUp } = useAuth();
+    const [location, setLotation] = useState<batata.LocationObjectCoords>()
+
+    const getPermission = async() => {
+        const { status } = await batata.requestBackgroundPermissionsAsync()
+        if(status !== 'granted') return false
+        return true
+    }
+
+    const getPosition = async() => {
+        const { coords } = await batata.getCurrentPositionAsync()
+        setLotation(coords)
+    }
+
+    const setup = async() =>{
+        const isAllowed =  await getPermission()
+        if(isAllowed) return getPosition()
+    }
+
+    useEffect(() => 
+        {
+            setup()
+        }
+        ,[])
+
+    if (location == undefined) return <View/>
 
     return (
-        <View>
+        <View style = {{flex: 1}}>
+            <MapView style = {{flex: 1}}
+                initialRegion={
+                    {
+                        latitude: location?.latitude,
+                        longitude:location?.longitude,
+                        latitudeDelta:0.01,
+                        longitudeDelta:0.01
+                    }
+                }/>
             <Header />
             <AuthForm
                 email={email}
